d0f6fb19a5cf6d6e04ae39d34705c5929c44c648
Tom Bourjala <tom.bourjala@gmail.com>
2021-11-16 16:24:51 +0100

    Iniit AirManager

---
+++ Source/AirManager/AirManager.h
@@ -0,0 +1,41 @@
+#include "../SmartList/SmartList.h"
+
+typedef struct runway runway;
+
+typedef enum{FLYING, WAITING_LANDING, LANDING, PARKING, WAITING_TAKEOFF, TAKEOFF} planeStatus;
+typedef enum{AIRLINER, BUSINESS, LIGHT} planeType;
+
+typedef struct{
+    char matriculation[7];
+    planeType type;
+    unsigned int passengers;
+    planeStatus status;
+    runway *targetRunway;
+}plane;
+
+typedef enum{STANDBY, LANDING, TAKEOFF} runwayStatus;
+typedef enum{SMALL, MEDIUM, LARGE} runwayType;
+
+typedef struct{
+    char id;
+    float length;
+    float width;
+    runwayType type;
+    unsigned int maxTakeofQueue;
+    list *takeofQueue;
+    plane *takeoff;
+}runway;
+
+typedef struct{
+    unsigned int parkingSize;
+    list *parkingPlanes;
+    list *runway;
+    list *planesInRange;
+}airport;
+
+plane* newPlane(char matriculation[7], planeType type, unsigned int passengers, planeStatus status);
+
+runway* newRunway(char id, float length, float width, runwayType type, unsigned int maxTakeofQueue);
+
+airport* newAirport(unsigned int parkingSize);
+void buildAirport(airport* airport, int numberOfSmallRunway, int numberOfMediumRunway, int numberOfLargeRunway);
