f16e39043e910c8111be651efb2827fbfac991a1
Tom Bourjala <tom.bourjala@gmail.com>
2021-11-22 21:26:15 +0100

    AirSim First patch

--- Source/AirManager/AirSim.c
+++ Source/AirManager/AirSim.c
@@ -1,12 +1,20 @@
-#include "AirSim.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "AirSim.h"
 
 sim_planeActor *newSimPlaneActor(char matriculation[7], planeType type, unsigned int passengers, unsigned int passengersMax, planeStatus status){
     sim_planeActor *planeActor = malloc(sizeof(sim_planeActor));
     planeActor->plane = newPlane(matriculation, type, passengers, passengersMax, status);
     planeActor->stateRemainTimeInMs = -1;
     return planeActor;
+}
+
+sim_planeActor *getSimPlaneActorInList(plane *plane, simulation *simulation){
+    for(int p = 0; p < simulation->planeActors->length; p++){
+        sim_planeActor *planeActor = getDataAtIndex(simulation->planeActors, p);
+        if(planeActor->plane == plane) return planeActor; 
+    }
+    return NULL;
 }
 
 simulation initSimulation(int parkingSize, int numberOfSmallRunway, int numberOfMediumRunway, int numberOfLargeRunway, int numberOfPlanes){
@@ -30,12 +38,33 @@ simulation initSimulation(int parkingSize, int numberOfSmallRunway, int numberOf
     return newSim;
 }
 
+void planeNextAction(airport *airport, sim_planeActor *planeActor){
+    switch (planeActor->plane->status)
+    {
+    case FLYING:
+        addPlaneToLandingQueue(airport, planeActor->plane);
+        planeActor->stateRemainTimeInMs = -1;
+        break;
+    case LANDING:
+        planeExitRunway(airport, planeActor->plane);
+        planeActor->stateRemainTimeInMs = 5000*(planeActor->plane->type + 1); //TIME WAITING IN PARKING
+        break;
+    case PARKING:
+        addPlaneToAFRQ(airport, planeActor->plane);
+        planeActor->stateRemainTimeInMs = -1;
+        break;
+    case TAKEOFF:
+        planeExitRunway(airport, planeActor->plane);
+        planeActor->stateRemainTimeInMs = 50000; //TIME FLYING BEFORE RETURN
+        break;
+    }
+}
+
 int main(){
     simulation simulation = initSimulation(10,2,3,2,4);
     airport *airport = simulation.airport;
     //Cycle
-    while (1)
-    {
+    while (1) {
         // Every runway resolve xor request resolve
         for(int rw = 0; rw <= airport->runways->length; rw++){
             runway *runway = getDataAtIndex(airport->runways, rw);
@@ -53,9 +82,22 @@ int main(){
                         grantTakeoffForRunway(airport, runway);
                         break;
                 }
+                if(TODO)
+                    getSimPlaneActorInList(simulation, runway->planeLT)->stateRemainTimeInMs = 500*runway->length;
             }
         }
+        
         // Planes get stateRemainTimeInMs decremented
+        for(int p = 0; p < simulation->planeActors->length; p++){
+            sim_planeActor *planeActor = getDataAtIndex(simulation->planeActors, p);
+            planeActor->stateRemainTimeInMs -= simulation.simulationSpeedInMs; 
+        }
+
         // stateRemainTimeInMs <= 0 state is updated 
+        for(int p = 0; p < simulation->planeActors->length; p++){
+            sim_planeActor *planeActor = getDataAtIndex(simulation->planeActors, p);
+            if(planeActor->stateRemainTimeInMs <= 0)
+                planeNextAction(airport, planeActor);
+        }
     }
 }
