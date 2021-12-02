7cba72cbb0c707aedea5892c668174cbb14544a6
Tom Bourjala <tom.bourjala@gmail.com>
2021-12-02 14:46:16 +0100

    Update WindowSize

--- Source/AirManager/AirSim.c
+++ Source/AirManager/AirSim.c
@@ -83,7 +83,7 @@ int main(){
     simulation simulation = initSimulation(20,1,2,3,50);
     simulation.simulationSpeedInMs = 5;
     airport *airport = simulation.airport;
-    initWindow(1400,1000);
+    initWindow(1800,1000);
     while (1) {
         updateAirportRenderer(simulation);
         printf("\n ╔════════════════════════════════════════════════╗");
